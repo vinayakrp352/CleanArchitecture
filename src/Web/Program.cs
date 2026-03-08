010971ee9c149e5fba877e1651d4805cbf6e7dba
Vinay <vinayakrp352@gmail.com>
2026-03-07 21:13:20 -0700

    Fix production startup: enable DB init and redirect root to API docs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -16,11 +16,9 @@ builder.AddWebServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    await app.InitialiseDatabaseAsync();
-}
-else
+await app.InitialiseDatabaseAsync();
+
+if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -35,15 +33,9 @@ app.UseStaticFiles();
 app.MapOpenApi();
 app.MapScalarApiReference();
 
-#if (!UseApiOnly)
-app.MapFallbackToFile("index.html");
-#endif
-
 app.UseExceptionHandler(options => { });
 
-#if (UseApiOnly)
-app.Map("/", () => Results.Redirect("/scalar"));
-#endif
+app.Map("/", () => Results.Redirect("/scalar/v1"));
 
 #if (UseAspire)
 app.MapDefaultEndpoints();
